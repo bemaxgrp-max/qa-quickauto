323fff805ae715dc81044e4b968fa416f66ababb
Mohammad Al Homsi <bemaxgrp@gmail.com>
2026-06-05 12:34:36 +0800

    feat: update app icons and splash screens for Android and iOS platforms

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.quickauto.app',
   appName: 'QUICK AUTO',
-  webDir: 'dist'
+  webDir: 'dist',
+  ios: {
+    swipeNavigationEnabled: true
+  }
 };
 
 export default config;
